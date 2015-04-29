1157b021d0d606796e9bba8799d51d820ebd1a4e
Saikat <rychwdh2@illinois.edu>
2015-04-29 03:08:50 -0500

    json output of mongodb query

--- index_new.php
+++ index_new.php
@@ -1,13 +1,29 @@
-<html>
-    <head>Welcome to mongo test </head>
-<body>
+<?php
+header('Content-Type: application/json');
+    if ( isset($_POST['username'])  && isset($_GET['type']) ) {
+        //echo "Welcome ". $_POST['name']. "<br />";
+        //echo "You are ". $_POST['age']. " years old.";
+        //exit();
+        getNews();
+    }
+?>
     <?php
         //phpinfo();
         //$dbhost = 'localhost';
-        $dbhost = '76.10.1.23';
-        $dbname = 'local';
-        // Connect to test database
-        $m = new Mongo("mongodb://$dbhost");
+
+        //getNewsCount();
+        getNews();
+        function connect() {
+            $dbhost = '76.10.1.23';
+            $dbname = 'local';
+            // Connect to test database
+            $m = new Mongo("mongodb://$dbhost");
+            return $m;
+        }
+
+        function getNewsCount() {
+            $dbname = 'local';
+        $m = connect();
         if ($m == NULL) {
             echo "<br>nerror opening DB</br>";
         } else {
@@ -15,6 +31,7 @@
         }
         $db = $m->$dbname;
         $c_users = $db->TweetStore;
+        $count_things = 0;
         if ($c_users == NULL) {
             echo "<br>TweetStore collection not found</br>";
         } else {
@@ -25,13 +42,41 @@
             // Get a count of documents in the things collection
             $count_things = $c_things->count();
             echo "<br>There are $count_things documents in the things collection.</br>";
-
-            $cursor = $c_things->find();
-            foreach ( $cursor as $id => $value ) {
-                echo "$id: ";
-                var_dump( $value );
+        }
+            return $count_things;
+        }
+        
+        function getNews() {
+            $dbname = 'local';
+        $m = connect();
+        if ($m == NULL) {
+            echo "<br>nerror opening DB</br>";
+        } else {
+            //echo "<br>mongdb conn established!!</br>";
+        }
+        $db = $m->$dbname;
+        $c_users = $db->TweetStore;
+        if ($c_users == NULL) {
+            echo "<br>TweetStore collection not found</br>";
+        } else {
+            $c_things = $db->TweetStore;
+            if ($c_things !=  NULL) {
+                //echo "<br>found TweetStore colelction</br>";
             }
+            $cursor = $c_things->find();
+            $count = 1;
+            echo "[";
+            foreach ( $cursor as $id => $value ) {
+                //if ( $count == 20 ) break;
+                echo json_encode($value);
+                echo ",";
+                $count++;
+                //var_dump( $value );
+            }
+            echo "{}";
+            echo "]";
+        }
+//        return json_encode($cursor);
+
         }
     ?>  
-</body>
-</html>
